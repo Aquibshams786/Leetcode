cecf570ae0ba30345180344890025744f1d1b772
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-08-25 20:05:43 +0530

    25 aug

--- 4.cpp
+++ 4.cpp
@@ -1,20 +1,30 @@
-///container with most water
+/// container with most water
 
-
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
-        int l=0, r=height.size()-1, ans=INT_MIN;
-        
-        while(l<r) {
-            int current_area=min(height[l], height[r]) * (r-l);
-            ans=max(ans, current_area);
-            if(height[l]<height[r])
+    int maxArea(vector<int> &height)
+    {
+        int l = 0, r = height.size() - 1, ans = INT_MIN;
+
+        while (l < r)
+        {
+            int current_area = min(height[l], height[r]) * (r - l);
+            ans = max(ans, current_area);
+            if (height[l] < height[r])
                 l++;
             else
                 r--;
         }
-        
+
         return ans;
     }
 };
+
+
+
+
+
+//25 AUG
+
+
