415c528773c23d3dcaed7533362ae5cd6b8e452f
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-08-26 19:46:51 +0530

    1971

--- allin1.cpp
+++ allin1.cpp
@@ -103,3 +103,48 @@ public:
         return 0;
     }
 };
+
+// 1971
+
+class Solution
+{
+public:
+    bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
+    {
+        vector<int> arr[n];
+        for (int i = 0; i < edges.size(); i++)
+        {
+            arr[edges[i][0]].push_back(edges[i][1]);
+            arr[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<int> bfs;
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(source);
+        vis[source] = 1;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for (auto it : arr[node])
+            {
+                if (!vis[it])
+                {
+                    q.push(it);
+                    vis[it] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < bfs.size(); i++)
+        {
+            if (bfs[i] == destination)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
