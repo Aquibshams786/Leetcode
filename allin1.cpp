4533ae1c4d5679c030fd93ebe3f54b4975075b5b
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-09-02 01:58:10 +0530

    1448

--- allin1.cpp
+++ allin1.cpp
@@ -226,3 +226,42 @@ public:
         return ans;
     }
 };
+
+// 108
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *tree(vector<int> nums, int start, int end)
+    {
+        if (start > end)
+        {
+            return NULL;
+        }
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        if (start == end)
+        {
+            return root;
+        }
+        root->left = tree(nums, start, mid - 1);
+        root->right = tree(nums, mid + 1, end);
+        return root;
+    }
+
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        TreeNode *ans = tree(nums, 0, nums.size() - 1);
+        return ans;
+    }
+};
