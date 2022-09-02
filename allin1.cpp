83ee116da7a1929e795305bbc68c274530e5a533
Aquibshams786 <aquib.shams.it23@heritageit.edu.in>
2022-09-02 15:25:53 +0530

    637

--- allin1.cpp
+++ allin1.cpp
@@ -265,3 +265,47 @@ public:
         return ans;
     }
 };
+
+// 637
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<double> averageOfLevels(TreeNode *root)
+    {
+        vector<double> ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            double n = q.size();
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *temp = q.front();
+                q.pop();
+                sum = sum + temp->val;
+                if (temp->right)
+                {
+                    q.push(temp->right);
+                }
+                if (temp->left)
+                {
+                    q.push(temp->left);
+                }
+            }
+            ans.push_back(sum / n);
+        }
+        return ans;
+    }
+};
