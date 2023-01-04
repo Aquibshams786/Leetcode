f6c12b12aab18640b0b7e7c2e41c416a39724c76
AQUIB SHAMS <66779777+Aquibshams786@users.noreply.github.com>
2023-01-05 02:07:12 +0530

    Time: 927 ms (5.10%), Space: 104.7 MB (12.41%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        map<int,int> m;
+        for(int i=0;i<tasks.size();i++){
+            m[tasks[i]]++;
+        }
+        for(auto i:m){
+            cout<<i.first<<":"<<i.second<<endl;
+        }
+        int steps=0;
+        for(auto i:m){
+            if(i.second==1) return -1;
+            else if(i.second%3==0){
+                steps+=(i.second/3);
+            }
+            else{
+                steps+=(i.second/3)+1;
+            }
+        }
+        return steps;
+    }
+};
