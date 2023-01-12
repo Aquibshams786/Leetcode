b6e4afdf8df12e5f8afe2378495c7019f2bf9389
AQUIB SHAMS <66779777+Aquibshams786@users.noreply.github.com>
2023-01-13 00:57:18 +0530

    Time: 122 ms (58.71%), Space: 54.9 MB (6.95%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i:m){
+            if(i.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
