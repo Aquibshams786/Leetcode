e55436b508a6d839482176a8fa908ac00fbc40f9
AQUIB SHAMS <66779777+Aquibshams786@users.noreply.github.com>
2023-03-29 00:32:18 +0530

    Time: 740 ms (38.26%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    set N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      
+      select max(salary) from Employee group by salary order by salary desc limit N,1
+      
+  );
+END
