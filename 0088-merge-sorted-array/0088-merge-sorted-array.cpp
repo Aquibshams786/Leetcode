ff27780ae6eca512ee7824a1c5cd7969f07d0fff
AQUIB SHAMS <66779777+Aquibshams786@users.noreply.github.com>
2024-01-10 22:43:59 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (40.86%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        while(nums1.size()!=m){
+        while(nums1.size()>m){
             nums1.pop_back();
         }
         int i=0;
-        while(n>0){
+        while(i<nums2.size()){
             nums1.push_back(nums2[i]);
             i++;
-            n--;
         }
         sort(nums1.begin(),nums1.end());
     }
