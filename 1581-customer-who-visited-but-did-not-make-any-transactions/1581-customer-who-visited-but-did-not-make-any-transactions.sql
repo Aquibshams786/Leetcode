88f32d76548448fc6b4ec040bc59aff5ae8a6e06
AQUIB SHAMS <66779777+Aquibshams786@users.noreply.github.com>
2024-02-02 00:49:18 +0530

    Time: 2632 ms (38.57%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,1 @@
+select visits.customer_id,count( visits.customer_id) as count_no_trans from visits left join transactions on visits.visit_id=transactions.visit_id where transactions.visit_id is NULL group by  visits.customer_id
